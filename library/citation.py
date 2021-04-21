a974e596e0477065804606eceebead56bf58d77b
Vladimir Kharchev <necrosovereign@gmail.com>
2021-04-21 17:36:28 +0200

    Reference processing logic

---
+++ library/citation.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+from enum import IntEnum, Enum
+from typing import Dict, List, Optional, Iterator, Any
+
+import regex
+
+
+class LastSeparator(IntEnum):
+    Comma = 0
+    Ampersand = 1
+    And = 2
+    CommaAnd = 3
+
+    def __str__(self) -> str:
+        return [", ", " & ", " and ", ", and"][self]
+
+
+class YearFormat(IntEnum):
+    ParenColon = 0
+    ParenPeriod = 1
+    Paren = 2
+    Period = 3
+    Colon = 4
+    Comma = 5
+
+    def has_paren(self) -> bool:
+        return self <= YearFormat.Paren
+
+    def terminator(self) -> str:
+        return [":", ".", "", ".", ":", ","][self]
+
+    def format_year(self, year: int) -> str:
+        result = str(year)
+        if self.has_paren():
+            result = "(" + result + ")"
+        return result + self.terminator()
+
+    def __str__(self) -> str:
+        return self.format_year(1998)
+
+
+class Options(Enum):
+    InitialsBefore = (
+        bool, "Place initials before surname (except first name)")
+    InitialsNoPeriod = (bool, "Write initials without abbreviating period")
+    LastNameSep = (LastSeparator, "Precede last name with:")
+    YearFormat = (YearFormat, "Format year as:")
+
+    def __init__(self, type: type, description: str):
+        self.type = type
+        self.description = description
+
+
+OptionsDict = Dict[Options, Any]
+
+
+class Author:
+    def __init__(self, surname: str, initials: str):
+        self.surname = surname
+        self.initials = initials.replace(" ", "")
+
+    def format_author(self, options: OptionsDict, first: bool):
+        if options[Options.InitialsNoPeriod]:
+            initials = self.initials.replace(".", "")
+        else:
+            initials = self.initials
+        if options[Options.InitialsBefore] and not first:
+            return initials + " " + self.surname
+        else:
+            return self.surname + ", " + initials
+
+
+class Reference:
+    def __init__(self, authors: List[Author], year: int, article: str):
+        self.authors = authors
+        self.year = year
+        self.article = article
+
+    def format_authors(self, options: OptionsDict):
+        if not self.authors:
+            return ""
+        formatted_authors = (author.format_author(options, i == 0)
+                             for i, author in enumerate(self.authors))
+        *authors, last_author = formatted_authors
+        if not authors:
+            return last_author
+        else:
+            return ", ".join(authors) + \
+                str(options[Options.LastNameSep]) + last_author
+
+    def format_reference(self, options: OptionsDict):
+        return self.format_authors(options) + " " + options[Options.YearFormat].format_year(self.year) + " " + self.article
+
+    @staticmethod
+    def parse(s: str) -> Optional['Reference']:
+        year_match = regex.search(r'\(?(\d+)\)?\S?', s)
+        year_start, year_end = year_match.span()
+        authors = s[:year_start]
+        year = int(year_match.group(1))
+        article = s[year_end:]
+        return Reference(Reference.parse_authors(authors), year, article)
+
+    @staticmethod
+    def parse_authors(s: str) -> List[Author]:
+        # try to separate the last author
+        # after the loop parts_rest and last_part will be comma-separated lists of surnames and initials
+        for lastsep in map(str, reversed(list(LastSeparator))):
+            parts_rest, sep, last_part = s.partition(lastsep)
+            if sep:
+                break
+        parts = [part for part in parts_rest.split(
+            ", ") + last_part.split(", ") if part]
+        return [author for author in Reference.extract_author(parts)]
+
+    @staticmethod
+    def extract_author(parts: List[str]) -> Iterator[Author]:
+        while parts:
+            try:
+                part = parts.pop(0)
+                find_surname = regex.search(
+                    r'[[:upper:]][[:lower:]].*[[:lower:]]', part)
+                if not find_surname:
+                    initials = part
+                    surname = parts.pop(0)
+                elif find_surname.span() == (0, len(part)):
+                    surname = part
+                    initials = parts.pop(0)
+                elif find_surname.start() == 0:
+                    surname = find_surname.group()
+                    initials = part[find_surname.end() + 1:]
+                else:
+                    surname = find_surname.group()
+                    initials = part[:find_surname.start() - 1]
+                yield(Author(surname, initials))
+            except IndexError as e:
+                raise StopIteration from e
