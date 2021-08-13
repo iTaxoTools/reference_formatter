991ec848b33ef90a5d65ebe881d1ee31d6260d5c
Vladimir Kharchev <necrosovereign@gmail.com>
2021-08-13 15:44:44 +0200

    HTML list iteration

--- library/handle_html.py
+++ library/handle_html.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import regex
-from typing import Tuple, Optional, NamedTuple
+from typing import Tuple, Optional, NamedTuple, Iterator
 
 
 class TagPosition(NamedTuple):
@@ -15,20 +15,33 @@ class LocatedTag(NamedTuple):
 
 
 def _find_tag(input: str, tag: str) -> Optional[TagPosition]:
-    tag_match = regex.search(r'<\s*' + tag + r'[^>]>', input)
+    tag_match = regex.search(r'<\s*' + tag + r'[^>]>', input, flags=regex.IGNORECASE)
     if tag_match:
         return TagPosition(tag_match.start(), tag_match.end())
     else:
         return None
 
 
-def _next_tag(input: str, tag: str) -> Optional[LocatedTag]:
-    tag_match = regex.search(r'<\s*(\w+)[^>]>', input)
+def _next_tag(input: str) -> Optional[LocatedTag]:
+    tag_match = regex.search(r'<\s*(\w+)[^>]>', input, flags=regex.IGNORECASE)
     if tag_match:
-        return LocatedTag(tag_match.group(1),
+        return LocatedTag(tag_match.group(1).casefold(),
                           TagPosition(tag_match.start(), tag_match.end()))
     else:
         return None
+
+
+class ListEntry(NamedTuple):
+    decoration: Optional[str]
+    content: str
+
+    @staticmethod
+    def construct(entry: str) -> ListEntry:
+        decoration_match = regex.fullmatch(r'(<\s*(\w+)[^>]>)(.*?)(</\2>)?', entry)
+        if decoration_match:
+            return ListEntry(decoration_match.group(1), decoration_match.group(3))
+        else:
+            return ListEntry(None, entry)
 
 
 class HTMLList():
@@ -45,6 +58,15 @@ class HTMLList():
         self._separate_preamble()
         self._detect_list_type()
 
+    def __iter__(self) -> Iterator[ListEntry]:
+        return self
+
+    def __next__(self) -> ListEntry:
+        if self._list_type == HTMLList.PARAGRAPHS:
+            return self._paragraphs_next()
+        else:
+            return self._list_next()
+
     def _separate_preamble(self) -> None:
         located_body = _find_tag(self._input, 'body')
         if not located_body:
@@ -57,4 +79,62 @@ class HTMLList():
             self._input = self._input[:located_body_end.start]
 
     def _detect_list_type(self) -> None:
-        ...
+        first_body_tag = _next_tag(self._input)
+        if not first_body_tag:
+            raise ValueError("The reference list is unstructured")
+        if first_body_tag.name == "ul":
+            self._list_type = HTMLList.UNORDERED
+        elif first_body_tag.name == "ol":
+            self._list_type = HTMLList.ORDERED
+        elif first_body_tag.name == "p":
+            self._list_type = HTMLList.PARAGRAPHS
+        else:
+            raise ValueError("Can't detect the structure of the reference list")
+
+    def _list_next(self) -> ListEntry:
+        li_tag = _find_tag(self._input, "li")
+        if not li_tag:
+            raise StopIteration
+        self._input = self._input[li_tag.end:]
+        close_li_tag = _find_tag(self._input, "/li")
+        next_li_tag = _find_tag(self._input, "li")
+        if not close_li_tag and not next_li_tag:
+            if self._list_type == HTMLList.UNORDERED:
+                end_list_tag = _find_tag(self._input, "/ul")
+            elif self._list_type == HTMLList.ORDERED:
+                end_list_tag = _find_tag(self._input, "/ol")
+            else:
+                assert False
+            if end_list_tag:
+                entry_content = self._input[:end_list_tag.start]
+            else:
+                entry_content = self._input
+            return ListEntry.construct(entry_content)
+        if close_li_tag:
+            entry_content = self._input[:close_li_tag.start]
+            self._input = self._input[close_li_tag.end:]
+        elif next_li_tag:
+            entry_content = self._input[:next_li_tag.start]
+            self._input = self._input[next_li_tag.start:]
+        else:
+            assert False
+        return ListEntry.construct(entry_content)
+
+    def _paragraphs_next(self) -> ListEntry:
+        p_tag = _find_tag(self._input, "p")
+        if not p_tag:
+            raise StopIteration
+        p_description = self._input[p_tag.start:p_tag.end]
+        self._input = self._input[p_tag.end:]
+        close_p_tag = _find_tag(self._input, "/p")
+        next_p_tag = _find_tag(self._input, "p")
+        if close_p_tag:
+            entry_content = p_description + self._input[:close_p_tag.end]
+            self._input = self._input[close_p_tag.end:]
+        elif next_p_tag:
+            entry_content = p_description + self._input[:next_p_tag.start]
+            self._input = self._input[next_p_tag.start:]
+        else:
+            entry_content = p_description + self._input
+            self._input = ""
+        return ListEntry.construct(entry_content)
