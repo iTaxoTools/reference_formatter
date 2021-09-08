65c3f73d7f273f052ac0ff200e700285851490d6
Vladimir Kharchev <necrosovereign@gmail.com>
2021-09-08 18:08:39 +0200

    Fixed relative paths bug

--- library/resources.py
+++ library/resources.py
@@ -2,10 +2,11 @@
 
 from typing import Any
 from pathlib import Path
+import sys
 # import importlib.resources
 
 # _resource_path = importlib.resources.files('itaxotools.concatenator') / "resources"
-_resource_path = Path("data")
+_resource_path = Path(sys.path[0]) / "data"
 
 
 def get_resource(path: Any) -> str:
